1d7ec442b11806f73880927b538307653f887077
Rameez Khan <rameezkhan.sa@gmail.com>
2019-03-11 14:08:30 +0200

    Add some environmnent functions

--- functions.ps1
+++ functions.ps1
@@ -1,3 +1,55 @@
 # Basic commands
 function which($name) { Get-Command $name -ErrorAction SilentlyContinue | Select-Object Definition }
 function touch($file) { "" | Out-File $file -Encoding ASCII }
+
+# Sudo
+function sudo() {
+    if ($args.Length -eq 1) {
+        start-process $args[0] -verb "runAs"
+    }
+    if ($args.Length -gt 1) {
+        start-process $args[0] -ArgumentList $args[1..$args.Length] -verb "runAs"
+    }
+}
+
+# Reload the Shell
+function Reload-Powershell {
+    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+    $newProcess.Arguments = "-nologo";
+    [System.Diagnostics.Process]::Start($newProcess);
+    exit
+}
+
+### Environment functions
+### ----------------------------
+
+# Reload the $env object from the registry
+function Refresh-Environment {
+    $locations = 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment',
+                 'HKCU:\Environment'
+
+    $locations | ForEach-Object {
+        $k = Get-Item $_
+        $k.GetValueNames() | ForEach-Object {
+            $name  = $_
+            $value = $k.GetValue($_)
+            Set-Item -Path Env:\$name -Value $value
+        }
+    }
+
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+}
+
+# Set a permanent Environment variable, and reload it into $env
+function Set-Environment([String] $variable, [String] $value) {
+    Set-ItemProperty "HKCU:\Environment" $variable $value
+    # Manually setting Registry entry. SetEnvironmentVariable is too slow because of blocking HWND_BROADCAST
+    #[System.Environment]::SetEnvironmentVariable("$variable", "$value","User")
+    Invoke-Expression "`$env:${variable} = `"$value`""
+}
+
+# Add a folder to $env:Path
+function Prepend-EnvPath([String]$path) { $env:PATH = $env:PATH + ";$path" }
+function Prepend-EnvPathIfExists([String]$path) { if (Test-Path $path) { Prepend-EnvPath $path } }
+function Append-EnvPath([String]$path) { $env:PATH = $env:PATH + ";$path" }
+function Append-EnvPathIfExists([String]$path) { if (Test-Path $path) { Append-EnvPath $path } }
