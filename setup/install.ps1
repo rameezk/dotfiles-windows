4a40d5839296f7773148f522177818a24d3b19ce
Rameez Khan <rameezkhan.sa@gmail.com>
2019-03-18 09:46:16 +0200

    Remove unzipping of branch

--- setup/install.ps1
+++ setup/install.ps1
@@ -50,13 +50,13 @@ function Unzip-File {
 
 Download-File "https://github.com/$account/$repo/archive/$branch.zip" $sourceFile
 if ([System.IO.Directory]::Exists($dotfilesInstallDir)) {[System.IO.Directory]::Delete($dotfilesInstallDir, $true)}
-Unzip-File $sourceFile $dotfilesTempDir
+# Unzip-File $sourceFile $dotfilesTempDir
 
-Push-Location $dotfilesInstallDir
-& .\bootstrap.ps1
-Pop-Location
+# Push-Location $dotfilesInstallDir
+# & .\bootstrap.ps1
+# Pop-Location
 
-$newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
-$newProcess.Arguments = "-nologo";
-[System.Diagnostics.Process]::Start($newProcess);
+# $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+# $newProcess.Arguments = "-nologo";
+# [System.Diagnostics.Process]::Start($newProcess);
 exit
