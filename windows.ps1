af432e4cc936852c36195a4e0fb585465f47012a
Rameez Khan <rameezkhan.sa@gmail.com>
2019-03-19 09:05:36 +0200

    Install Ubuntu

--- windows.ps1
+++ windows.ps1
@@ -11,5 +11,16 @@ if (!(Verify-Elevated)) {
 
 Write-Host "[..] Enabling Bash on Windows"
 Enable-WindowsOptionalFeature -Online -All -FeatureName "Microsoft-Windows-Subsystem-Linux" -NoRestart -WarningAction SilentlyContinue | Out-Null
+Write-Host "[..] Downloading Ubuntu"
+$ubuntuTempDir = Join-Path $env:TEMP "ubuntu"
+Push-Location $ubuntuTempDir
 $ProgressPreference = 'SilentlyContinue'
-Invoke-WebRequest -Uri "https://aka.ms/wsl-ubuntu-1604" -OutFile Ubuntu.appx -UseBasicParsing
+Invoke-WebRequest -Uri "https://aka.ms/wsl-ubuntu-1804" -OutFile Ubuntu.appx -UseBasicParsing
+Write-Host "[..] Installing Ubuntu"
+Add-AppxPackage -Path .\Ubuntu.appx
+Pop-Location
+
+Write-Host "[..] Done"
+Start-Sleep -s 5
+
+exit
